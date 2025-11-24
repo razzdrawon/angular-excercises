13810434b17f37ac67d055c24be455fd24bfdc92
Ricardo Padilla <pagr.dr@gmail.com>
2025-11-23 23:59:17 -0500

    feat: simplify navigation and add help modal
    - Remove placeholder Angular content from main view
    - Add module navigation tabs
    - Add help button with modal containing Angular resources
    - Keep original content accessible via help modal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -39,8 +39,117 @@ export const routes: Routes = [
         loadComponent: () => import('./exercises/module-1/practice/practice-1-2-shopping-cart.component').then(m => m.Practice12ShoppingCartComponent)
       },
       {
+        path: 'practice-1-3',
+        loadComponent: () => import('./exercises/module-1/practice/practice-1-3-effects.component').then(m => m.Practice13EffectsComponent)
+      },
+      {
         path: '',
         redirectTo: 'exercise-1-1',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'module-2',
+    loadComponent: () => import('./modules/module-2/module-2-container.component').then(m => m.Module2ContainerComponent),
+    children: [
+      {
+        path: 'exercise-2-1',
+        loadComponent: () => import('./exercises/module-2/exercise-2-1-standalone-library.component').then(m => m.Exercise21StandaloneLibraryComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'exercise-2-1',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'module-3',
+    loadComponent: () => import('./modules/module-3/module-3-container.component').then(m => m.Module3ContainerComponent),
+    children: [
+      {
+        path: 'exercise-3-1',
+        loadComponent: () => import('./exercises/module-3/exercise-3-1-optimized-list.component').then(m => m.Exercise31OptimizedListComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'exercise-3-1',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'module-4',
+    loadComponent: () => import('./modules/module-4/module-4-container.component').then(m => m.Module4ContainerComponent),
+    children: [
+      {
+        path: 'exercise-4-1',
+        loadComponent: () => import('./exercises/module-4/exercise-4-1-permissions-roles.component').then(m => m.Exercise41PermissionsRolesComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'exercise-4-1',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'module-5',
+    loadComponent: () => import('./modules/module-5/module-5-container.component').then(m => m.Module5ContainerComponent),
+    children: [
+      {
+        path: 'exercise-5-1',
+        loadComponent: () => import('./exercises/module-5/exercise-5-1-multi-step-form.component').then(m => m.Exercise51MultiStepFormComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'exercise-5-1',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'module-6',
+    loadComponent: () => import('./modules/module-6/module-6-container.component').then(m => m.Module6ContainerComponent),
+    children: [
+      {
+        path: 'exercise-6-1',
+        loadComponent: () => import('./exercises/module-6/exercise-6-1-multi-tenant-routing.component').then(m => m.Exercise61MultiTenantRoutingComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'exercise-6-1',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'module-7',
+    loadComponent: () => import('./modules/module-7/module-7-container.component').then(m => m.Module7ContainerComponent),
+    children: [
+      {
+        path: 'exercise-7-1',
+        loadComponent: () => import('./exercises/module-7/exercise-7-1-testing-suite.component').then(m => m.Exercise71TestingSuiteComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'exercise-7-1',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'module-8',
+    loadComponent: () => import('./modules/module-8/module-8-container.component').then(m => m.Module8ContainerComponent),
+    children: [
+      {
+        path: 'exercise-8-1',
+        loadComponent: () => import('./exercises/module-8/exercise-8-1-ssr-pwa.component').then(m => m.Exercise81SsrPwaComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'exercise-8-1',
         pathMatch: 'full'
       }
     ]
